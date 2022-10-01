f8de1aff7e6c91c0b9c6dad010f0226280e6375e
Filipe Botelho <filipedesousab@gmail.com>
2022-10-01 00:40:39 -0300

    Add TypeError

--- lib/juno_ruby.rb
+++ lib/juno_ruby.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "juno_ruby/version"
 require_relative "juno_ruby/configuration"
+require_relative "juno_ruby/errors"
+require_relative "juno_ruby/version"
 
 module JunoRuby
   class << self
